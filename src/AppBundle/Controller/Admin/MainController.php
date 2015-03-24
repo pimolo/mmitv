df5e876098b2ac96a07baecc7f61de5f753e5ed9
Rémi Andrieux <pimolo10300@gmail.com>
2015-03-24 22:54:23 +0100

    Integrate back markup & styles & twig inheritance

--- src/AppBundle/Controller/Admin/MainController.php
+++ src/AppBundle/Controller/Admin/MainController.php
@@ -9,22 +9,30 @@ use AppBundle\Entity\Video;
 class MainController extends Controller
 {
     /**
-     * @Route("/", name="app_admin_homepage")
+     * @Route("/", name="app_admin_accueil")
      */
     public function indexAction()
     {
-        $userManager = $this->get('fos_user.user_manager');
-        $users = $userManager->findUsers();
-        return $this->render('AppBundle:Admin:main/index.html.twig', array('redacteurs' => $users));
+        return $this->render('AppBundle:Admin:main/index.html.twig');
     }
 
     /**
-     * @Route("/videos", name="app_admin_homepage")
+     * @Route("/videos", name="app_admin_videos")
      */
     public function videosAction()
     {
         $videos = $this->getDoctrine()->getRepository('AppBundle:Video')->findAll();
 
         return $this->render('AppBundle:Admin:main/video.html.twig', array('videos' => $videos));
+    }
+
+    /**
+     * @Route("/utilisateurs", name="app_admin_utilisateurs")
+     */
+    public function usersAction()
+    {
+        $userManager = $this->get('fos_user.user_manager');
+        $users = $userManager->findUsers();
+        return $this->render('AppBundle:Admin:main/users.html.twig', array('redacteurs' => $users));
     }
 }
