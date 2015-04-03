cef86be2bec0f25a2e26cef8ad06a8dc800fb0cb
Rémi Andrieux <pimolo10300@gmail.com>
2015-04-03 02:20:27 +0200

    Ajout de video côté serveur (+ sûr)

--- src/AppBundle/Controller/Admin/AjaxController.php
+++ src/AppBundle/Controller/Admin/AjaxController.php
@@ -15,9 +15,24 @@ class AjaxController extends Controller
     /**
      * @Route("/ajax-tester", name="app_admin_ajax_tester")
      */
-    public function testAction()
+    public function testAction(Request $request)
     {
-        return new Response("C'est ok", 200, array('Content-Type', 'text/plain'));
+        $id = $request->query->get('id');
+        $infos = file_get_contents('http://www.youtube.com/oembed?url=http://www.youtube.com/watch?v='.$id.'&format=json');
+        // $title = $infos->title;
+        // $infos = Request::create(
+        //     "http://www.youtube.com/oembed",
+        //     "GET",
+        //     array(
+        //         "url" => "http://www.youtube.com/watch?v=".$id,
+        //         "format" => "json"
+        //     ),
+        //     array(),
+        //     array(),
+        //     array("Content-Type" => "application/json")
+        // );
+        // $infos = json_encode(http_get("http://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=".$id."&format=json", array("Content-Type" => "application/json")));
+        return new Response($infos, 200, array());
     }
 
     /**
@@ -41,5 +56,30 @@ class AjaxController extends Controller
         $em->flush();
 
         return new Response('Vidéos bien ajoutées !', 200, array('Content-Type' => 'text/plain'));
+    }
+
+    /**
+     * @Route("/add-youtube-video", name="app_admin_add_video")
+     * @Method("POST")
+     */
+    public function addYoutubeVideoAction(Request $request)
+    {
+        $id = $request->request->get('url');
+        $infos = json_decode(file_get_contents('http://www.youtube.com/oembed?url='.$id.'&format=json'));
+        $title = $infos->title;
+        $author = $infos->author_name;
+        $duration = 120;
+        $embed_code = $infos->html;
+
+        $video = new Video();
+        $video->setTItle($title);
+        $video->setAuthor($author);
+        $video->setDuration($duration);
+        $video->setEmbedCode($embed_code);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($video);
+        $em->flush();
+
+        return new Response('Vidéo '.$infos->provider_name.' bien ajoutée !', 200, array('Content-Type' => 'text/html'));
     }
 }
--- src/AppBundle/Resources/public/js/oembed.js
+++ src/AppBundle/Resources/public/js/oembed.js
@@ -1,39 +1,56 @@
 $('document').ready(function() {
-    $('input').eq(0).change(function() {
-        var $video = $(this).val();
+    var $video;
+    $('#url').change(function() {
+        $video = $(this).val();
+        // prévisualisation
         $.ajax({
             url: 'http://noembed.com/embed',
             data: { url: $video },
             dataType: 'json',
             success: function(data) {
+                $('#infos div').html('');
                 if (data.error) {
                     $('input').after($('<span />').html("L'url n'est pas supportée...").css('color', 'red'));
                 } else {
                     localStorage.setItem(localStorage.length++, JSON.stringify(data));
-                    $('#titre').append(data.title);
-                    $('#auteur').append(data.author_name);
-                    $('#video').append(data.html);
-                    $('<button />').html('Ajouter').appendTo($('#infos'));
+                    $('#titre span').html(data.title);
+                    $('#auteur span').html(data.author_name);
+                    $('#video span').html(data.html);
+                    $('#add').show();
                     console.log(data);
                 }
             }
         });
     });
-    $('body').on('click', 'button', function(e) {
-        console.log('test');
-        for (var i = localStorage.length - 1; i >= 0; i--) {
-            var infos = JSON.parse(localStorage.getItem(i));
-            $.ajax({
-                url: 'add-video',
-                method: 'POST',
-                data: {
-                    title: infos.title,
-                    author: infos.author_name,
-                    duration: 120,
-                    html: infos.html
-                },
-            });
-            localStorage.clear();
-        }
+
+        // enregistrement
+    $('#add').click(function() {
+        $.ajax({
+            url: 'add-youtube-video',
+            method: 'POST',
+            data: 'url='+$video,
+            dataType: 'json',
+            success: function(data) {
+                $('#add').after(data);
+            }
+        });
     });
+
+    // $('body').on('click', 'button', function(e) {
+    //     console.log('test');
+    //     for (var i = localStorage.length - 1; i >= 0; i--) {
+    //         var infos = JSON.parse(localStorage.getItem(i));
+    //         $.ajax({
+    //             url: 'add-video',
+    //             method: 'POST',
+    //             data: {
+    //                 title: infos.title,
+    //                 author: infos.author_name,
+    //                 duration: 120,
+    //                 html: infos.html
+    //             },
+    //         });
+    //         localStorage.clear();
+    //     }
+    // });
 });
